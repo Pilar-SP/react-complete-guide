90fc16e89ceaebb8cc9fddce8325330baf3200a6
Pilar Sanchez <pilar.sanchez@florence.co.uk>
2022-10-26 18:32:18 +0100

    My first state in React

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,21 @@
-import React from "react"; // No needed but pasted to rememeber that jsx html elements are syntactic sugar from the original React.createElement()
+import React, { useState } from "react"; // No needed but pasted to rememeber that jsx html elements are syntactic sugar from the original React.createElement()
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTtitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTtitle("Updated!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {title} </h2>
         <div className="expense-item__price">$ {props.amount} </div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
