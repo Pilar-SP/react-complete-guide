af319435a8ca00c7e933776d9ceea25161957b6e
Pilar Sanchez <pilar.sanchez@florence.co.uk>
2022-10-30 18:07:57 +0000

    Continue with useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"; // No needed but pasted to rememeber that jsx html elements are syntactic sugar from the original React.createElement()
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -27,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
