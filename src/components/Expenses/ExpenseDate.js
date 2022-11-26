ce2cada24ce11438ed46dbbb0de33b88a8eab192
Pilar Sanchez <pilar.sanchez@florence.co.uk>
2022-11-26 15:43:43 +0000

    Expense tracker finished

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -3,14 +3,14 @@ import "./ExpenseDate.css";
 const ExpenseDate = (props) => {
   // helper variables or constants that may hold logic
 
-  const month = props.date.toLocaleString("en-GB", { month: "long" });
+  const month = props.date.toLocaleString("en-GB", { month: "short" });
   const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
     <div className="expense-date">
-      <div className="expense-date__month">{month}</div>
       <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
     </div>
   );
